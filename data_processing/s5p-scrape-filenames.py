fb537e0c77674ed4196ba3d3ea19253813ed04f5
Blair Jones <bjonesneu@gmail.com>
2021-11-05 00:37:44 +0000

    version of files to scrape .nc names for Russian region

---
+++ data_processing/s5p-scrape-filenames.py
@@ -0,0 +1,212 @@
+import argparse
+from functools import partial
+import geopandas
+from multiprocessing.pool import ThreadPool, Pool
+from os import cpu_count, makedirs
+from os.path import exists
+from pathlib import Path
+import rioxarray
+from sentinelsat.sentinel import (
+    SentinelAPI,
+    geojson_to_wkt,
+    read_geojson,
+)
+import sys
+from tqdm import tqdm
+import xarray as xr
+
+from s5p_tools_bcj import (
+    fetch_product,
+    DHUS_USER,
+    DHUS_PASSWORD,
+    DHUS_URL,
+    DOWNLOAD_DIR,
+    EXPORT_DIR,
+    PROCESSED_DIR
+)
+
+
+def main(
+    producttype,
+    aoi,
+    date,
+    qa,
+    unit,
+    resolution,
+    chunk_size,
+    num_threads,
+    num_workers,
+):
+
+    api = SentinelAPI(DHUS_USER, DHUS_PASSWORD, DHUS_URL)
+
+    tqdm.write("\nRequesting products\n")
+
+    query_body = {
+        "date": date,
+        "platformname": "Sentinel-5 Precursor",
+        "producttype": producttype
+    }
+
+    # query database
+    if aoi is None:
+        products = api.query(**query_body)
+    else:
+        footprint = geojson_to_wkt(read_geojson(Path(aoi)))
+        products = api.query(footprint, **query_body)
+
+    # display results
+    tqdm.write(
+        (
+            "Number of products found: {number_product}\n"
+            "Total products size: {size:.2f} GB\n"
+        ).format(
+            number_product=len(products),
+            size=api.get_products_size(products)
+            )
+    )
+
+    # list of uuids for each product in the query
+    ids_request = list(products.keys())
+    
+    if len(ids_request) == 0:
+        tqdm.write("Done!")
+        sys.exit(0)
+        
+    # list of downloaded filenames urls
+    filenames = [
+        f"{products[file_id]['title']}.nc"
+        for file_id in ids_request
+    ]
+
+    # write names to file
+    with open("./resources/nc_file_list.txt", "w") as outfile:
+        outfile.write("\n".join(str(item) for item in filenames))
+
+    tqdm.write("Finished scraping methane files!")
+    sys.exit(0)
+    return
+               
+                      
+    # list of downloaded filenames urls
+    filenames = [
+        DOWNLOAD_DIR / f"{products[file_id]['title']}.nc"
+        for file_id in ids_request
+    ]
+                      
+    makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+    with ThreadPool(num_threads) as pool:
+        pool.map(
+            partial(
+                fetch_product,
+                api=api,
+                products=products,
+                download_dir=DOWNLOAD_DIR
+            ),
+            ids_request)
+
+        pool.close()
+        pool.join()
+
+        
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description=(
+            "Request, download and process Sentinel data from Copernicus access hub. "
+            "Create a processed netCDF file binned by time, latitude and longitude"
+        )
+    )
+
+    # Product type: Used to perform a product based search
+    # Possible values are
+    #   L2__O3____
+    #   L2__NO2___
+    #   L2__SO2___
+    #   L2__CO____
+    #   L2__CH4___
+    #   L2__HCHO__
+    #   L2__AER_AI
+    #   L2__CLOUD_
+    parser.add_argument("product", help="Product type", type=str)
+
+    # Date: Used to perform a time interval search
+    # The general form to be used is:
+    #       date=(<timestamp>, <timestamp>)
+    # where < timestamp > can be expressed in one of the following formats:
+    #   yyyyMMdd
+    #   yyyy-MM-ddThh:mm:ssZ
+    #   yyyy-MM-ddThh:mm:ss.SSSZ(ISO8601 format)
+    #   NOW
+    #   NOW-<n>MINUTE(S)
+    #   NOW-<n>HOUR(S)
+    #   NOW-<n>DAY(S)
+    #   NOW-<n>MONTH(S)
+    parser.add_argument(
+        "--date",
+        help="date used to perform a time interval search",
+        nargs=2,
+        type=str,
+        default=("NOW-24HOURS", "NOW"),
+    )
+
+    # Area of interest: The url of the area of interest (.geojson)
+    parser.add_argument(
+        "--aoi", help="path to the area of interest (.geojson)", type=str
+    )
+
+    # Unit: Unit conversion
+    parser.add_argument("--unit", help="unit conversion", type=str, default="mol/m2")
+
+    # qa value: Quality value threshold
+    parser.add_argument("--qa", help="quality value threshold", type=int, default=50)
+
+    # resolution: Spatial resolution in arc degrees
+    parser.add_argument(
+        "--resolution",
+        help="spatial resolution in arc degrees",
+        nargs=2,
+        type=float,
+        default=(0.01, 0.01),
+    )
+
+    # chunk-size:
+    parser.add_argument(
+        "--chunk-size",
+        help="dask chunk size along the time dimension",
+        type=int,
+        default=256,
+    )
+
+    # num-threads:
+    parser.add_argument(
+        "--num-threads",
+        help="number of threads spawned for L2 download",
+        type=int,
+        default=4,
+    )
+
+    # num-workers:
+    parser.add_argument(
+        "--num-workers",
+        help="number of workers spawned for L3 conversion",
+        type=int,
+        default=cpu_count(),
+    )
+
+    args = parser.parse_args()
+
+    main(
+        producttype=args.product,
+        aoi=args.aoi,
+        date=args.date,
+        qa=args.qa,
+        unit=args.unit,
+        resolution=args.resolution,
+        chunk_size=args.chunk_size,
+        num_threads=args.num_threads,
+        num_workers=args.num_workers,
+    )
